27a4786458a446bc9a67c466f7af0607d180e15a
avelagapudi <anupamav@auction.com>
2016-07-22 16:07:14 -0700

    updates

--- hack/www/js/index.js
+++ hack/www/js/index.js
@@ -33,7 +33,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        //app.receivedEvent('deviceready');
+         navigator.geolocation.getCurrentPosition(app.onSuccess, app.onError);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -45,5 +46,29 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    },
+
+     onSuccess: function(position){
+        var longitude = position.coords.longitude;
+        var latitude = position.coords.latitude;
+        var latLong = new google.maps.LatLng(latitude, longitude);
+
+        var mapOptions = {
+            center: latLong,
+            zoom: 13,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    
+        var marker = new google.maps.Marker({
+              position: latLong,
+              map: map,
+              title: 'my location'
+          });
+    },
+    
+    onError: function(error){
+        alert("the code is " + error.code + ". \n" + "message: " + error.message);
+    },
 };
